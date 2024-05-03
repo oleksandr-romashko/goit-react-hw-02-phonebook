f8b3ba34c4ec1529b8ebe8b1e21740951a69e937
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-05-03 02:49:08 +0200

    Add ContactList component

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+import React from 'react';
+import { List } from './ContactList.styled';
+
+/**
+ * Render the list of contacts.
+ * Displays default message when no no contacts.
+ * @param {object[]} [props.contacts] Contacts array.
+ * @returns {React.Component} List of contacts or default message.
+ */
+export const ContactList = ({ contacts }) =>
+  contacts && contacts.length > 0 ? (
+    <List>
+      {contacts.map(contact => (
+        <li key={contact.id}>{contact.name}</li>
+      ))}
+      {/* <li>Rosie Simpson</li>
+        <li>Hermione Kline</li>
+        <li>Eden Clements</li> */}
+    </List>
+  ) : (
+    'There is no contacts'
+  );
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+/**
+ * Styled list to display contacts.
+ */
+export const List = styled('ul')({
+  marginLeft: '52px',
+  listStyleType: 'disc',
+});
