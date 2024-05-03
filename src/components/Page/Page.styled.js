37b94df8ed5bb6a197bb57d3a8fcc6eeeaf47ec3
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-05-03 02:19:37 +0200

    Add Page component

---
+++ src/components/Page/Page.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+/**
+ * Styled page container to contain main page elements.
+ */
+const Page = styled('div')({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  fontSize: '32px',
+  lineHeight: '1.15',
+  color: '#000000',
+});
+
+export default Page;
