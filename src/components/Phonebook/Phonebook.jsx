59cf8321545954cdc1382f68b394fe68d57da250
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-05-14 11:03:11 +0200

    Refactor contact deletion using single filter statement

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -14,7 +14,12 @@ import textToNormalizedWordsArray from 'components/helpers/textToNormalizedWords
  */
 class Phonebook extends Component {
   #defaultState = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   }
 
@@ -74,9 +79,7 @@ class Phonebook extends Component {
    * @param {string} number Phone number of the contact. 
    */
   deleteContactById = (id) => {
-    const index = this.state.contacts.findIndex(el => el.id === id);
-    const contacts = this.state.contacts.filter((_, idx) => idx !== index);
-    this.setState({ contacts: contacts });
+    this.setState({ contacts: this.state.contacts.filter(el => el.id !== id)});
   }
 
   /**
